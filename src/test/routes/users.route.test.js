f36cb0871bc491a8b81f019de1c3f3d2b0fb2680
Maxime Ishimwe <mxmishimwe5@gmail.com>
2020-08-26 11:45:06 +0200

    chores: added user route tests

---
+++ src/test/routes/users.route.test.js
@@ -0,0 +1,91 @@
+import chai, {assert} from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../../index';
+import dotEnv from 'dotenv';
+
+dotEnv.config();
+
+chai.use(chaiHttp);
+
+const tokens = {
+	adminToken: null,
+	normalUserToken: null,
+	commentToken: null,
+	postToken: null,
+}
+
+const fakeUsername = process.env.FAKE_USERNAME;
+const fakeUser_Email = process.env.FAKE_USER_EMAIL;
+const fakeUser_Pass = process.env.FAKE_USER_PASS;
+const fakeRole = process.env.FAKE_ROLE;
+
+const normalUser_Email = process.env.NORMAL_USER_EMAIL;
+const normalUser_Pass = process.env.NORMAL_USER_PASS;
+const normalUser_Role = process.env.NORMAL_USER_ROLE;
+
+const guestUser_Role = process.env.GUEST_USER_ROLE;
+
+const adminUser_Email = process.env.ADMIN_EMAIL;
+const adminUser_Pass = process.env.ADMIN_PASS;
+const adminUser_Role = process.env.ADMIN_USER_ROLE;
+
+describe('Tests to API user routes (from admin page)', () => {
+	it('(200 Success) GET /getUsers to get all users using admin token (usertoken)', done => {
+		chai.request(server)
+			.post('/login')
+			.set('email', adminUser_Email)
+			.set('password', adminUser_Pass)
+			.end((err, res) => {
+				if (err) done(err);
+				tokens.adminToken = res.body.token;
+				chai.request(server)
+					.get('/getUsers')
+					.set('usertoken', tokens.adminToken)
+					.end((err, res) => {
+						if (err) done(err);
+						assert.equal(res.status, 200);
+						done();
+					})
+			})
+	});
+
+	it('(403 Unauthorized) GET /getUsers with a normal user token (usertoken)', done => {
+		chai.request(server)
+			.post('/login')
+			.set('email', normalUser_Email)
+			.set('password', normalUser_Pass)
+			.end((err, res) => {
+				if (err) done(err);
+				tokens.normalUserToken = res.body.token;
+				chai.request(server)
+					.get('/getUsers')
+					.set('usertoken', tokens.normalUserToken)
+					.end((err, res) => {
+						if (err) done(err);
+						assert.equal(res.status, 403);
+						done();
+					})
+			})
+	});
+
+	it('(403 Unauthorized) GET /getUsers to get all users but without a user token', done => {
+		chai.request(server)
+			.get('/getUsers')
+			.end((err, res) => {
+				if (err) done(err);
+				assert.equal(res.status, 403);
+				done();
+			});
+	})
+
+	it('(403 Unauthorized) GET /getUsers to get all users with an invalid user token', done => {
+		chai.request(server)
+			.get('/getUsers')
+			.set('usertoken', 'jkljkljkljj')
+			.end((err, res) => {
+				if (err) done(err);
+				assert.equal(res.status, 403);
+				done();
+			});
+	})
+})

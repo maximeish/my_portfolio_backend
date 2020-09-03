b06bf699b81134bf4825007baea4a717cdf27271
Maxime Ishimwe <mxmishimwe5@gmail.com>
2020-09-03 12:42:08 +0200

    fix: cannot sign up users with the same email or username

--- src/controllers/signup.controller.js
+++ src/controllers/signup.controller.js
@@ -7,6 +7,8 @@ dotEnv.config();
 
 
 export const signup = (req, res) => {
+    let emails = [];
+    let usernames = [];
     //Get provided values then sign user up
     let { username, email, password, role, subscribed } = req.body;
     if (username && email && password && role && subscribed) {
@@ -27,7 +29,7 @@ export const signup = (req, res) => {
             jwt.sign({ _id: uniqId, username, email, password, role, subscribed, date_joined: dateJoined }, process.env.SECRET_KEY, (err, token) => {
                 if(token) {
                     //Create an instance of User model
-                    const user = new User({
+                    const newUser = new User({
                         _id: uniqId,
                         userToken: token,
                         username,
@@ -39,21 +41,72 @@ export const signup = (req, res) => {
                     });
 
 
-                    //Save the user data to the database
-                    user.save()
-                        .then(result => {
-                            return res.status(200).json({
-                                message: "Sign up successful",
-                                userToken: result.userToken,
-                                userRole: result.role
-                            });
+                    //Check if there are no users with the same email and 
+                    // username the Save the user data to the database
+                    User.find({})
+                        .exec()
+                        .then(async users => {
+                            if (users.length > 0) {
+                                for (let user of users) {
+                                    emails.push(user.email);
+                                    usernames.push(user.username);
+                                }
+
+                                if (!emails.includes(email)) {
+                                    if (!usernames.includes(username)) {
+                                        const saveUser =  newUser.save()
+                                            
+                                            saveUser.then(result => {
+                                                return res.status(200).json({
+                                                    message: "Sign up successful",
+                                                    userToken: result.userToken,
+                                                    userRole: result.role
+                                                });
+                                            })
+                                            .catch(err => {
+                                                return res.status(500).json({
+
+                                                    Error: err
+                                                });
+                                            })
+                                    } else {
+                                        return res.status(409).json({
+                                            status: "Username taken",
+                                            message: "This username is already taken. Try choosing another one"
+                                        })
+                                    }
+                                } else {
+                                    return res.status(409).json({
+                                        status: "Email taken",
+                                        message: "This email is already taken. Try choosing another one"
+                                    })
+                                }
+
+                            } else {
+                                await newUser.save()
+                                    .then(result => {
+                                        return res.status(200).json({
+                                            message: "Sign up successful",
+                                            userToken: result.userToken,
+                                            userRole: result.role
+                                        });
+                                    })
+                                    .catch(err => {
+                                        return res.status(500).json({
+                                            status: "caught from newUser.save()",
+                                            Error: err
+                                        });
+                                    }); 
+                            }
                         })
                         .catch(err => {
                             return res.status(500).json({
+                                status: "caught from User.find({})",
                                 Error: err
-                            });
-                        }); 
+                            })
+                        })
                 }
+                
                 if (err) {
                     return res.status(500).json({
                         status: 'Server Error',
@@ -62,14 +115,14 @@ export const signup = (req, res) => {
                 }
             });
         } else {
-            res.status(400).json({
+            return res.status(400).json({
                 status: 'Bad Request',
                 message: 'Invalid role. Should be admin or user'
             });
         }
     }
 
-    else res.status(400).json({
+    else return res.status(400).json({
         status: 'Bad Request',
         message: 'Please, provide all details for a user (username, email, password, role) and make sure subscribed is yes or no'
     });

4554cefaa835fdb442e439f219e19728ce40185c
Maxime Ishimwe <mxmishimwe5@gmail.com>
2020-09-06 14:14:23 +0200

    feature: added functionality for normal users to perform CRUD operations to their own posts

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -16,11 +16,41 @@ export const getPosts = (req, res) => {
                     message: "You need to provide a valid token"
                 });
 
-            if (authUser.role !== process.env.ADMIN_USER_ROLE) 
-                return res.status(403).json({
-                    status: "Unauthorized",
-                    message: "You are not allowed to view this page"
+            if (authUser.role === process.env.NORMAL_USER_ROLE) {
+                Post.find({}, (err, docs) => {
+                    if (err) {
+                        return res.status(500).json({
+                            Error: err
+                        })
+                    }
+
+                    if (docs.length > 0) {
+                        let myDocs = [];
+                        
+                        docs.forEach(doc => {
+                            if (doc.author === authUser.username) myDocs.push(doc);
+                        })
+
+                        if (myDocs.length > 0) {
+                            return res.status(200).json({
+                                status: "Success",
+                                postsCount: myDocs.length,
+                                posts: myDocs
+                            })
+                        } else {
+                            return res.status(200).json({
+                                status: "Success",
+                                message: "You have not added any posts yet"
+                            }) 
+                        }
+                    } else {
+                        return res.status(200).json({
+                            status: "Success",
+                            message: "You have not added any posts yet"
+                        })
+                    }
                 });
+            }
 
             if (authUser.role === process.env.ADMIN_USER_ROLE) {
                 Post.find({}, (err, docs) => {
@@ -55,8 +85,8 @@ export const getPosts = (req, res) => {
 
 export const addPost = (req, res) => {
     const { usertoken } = req.headers;
-    const { title, body, author } = req.body;
-    if (usertoken && title && body && author) {
+    const { title, body } = req.body;
+    if (usertoken && title && body) {
         jwt.verify(usertoken, process.env.SECRET_KEY, (err, authUser) => {
             if (err) {
                 return res.status(403).json({
@@ -65,19 +95,12 @@ export const addPost = (req, res) => {
                 });
             }
 
-            if (authUser.role !== process.env.ADMIN_USER_ROLE) {
-                return res.status(403).json({
-                    status: "Unauthorized",
-                    message: "You are not allowed to use this feature"
-                });
-            }
-
-            if (authUser.role === process.env.ADMIN_USER_ROLE) {
+            if (authUser.role === process.env.ADMIN_USER_ROLE || authUser.role === process.env.NORMAL_USER_ROLE) {
                 const post = new Post({
                     _id: new mongoose.Types.ObjectId(),
                     title,
                     body,
-                    author,
+                    author: authUser.username.toString(),
                     date_posted: new Date()
                 });
 
@@ -93,13 +116,18 @@ export const addPost = (req, res) => {
                             Error: err
                         })
                     })
-            };
+            } else {
+                return res.status(403).json({
+                    status: "Unauthorized",
+                    message: "You are not allowed to use this feature"
+                });
+            }
         });
     }
 
     else res.status(400).json({
         status: 'Bad Request',
-        message: 'Please, provide all details (usertoken, title, body, author)'
+        message: 'Please, provide all details (usertoken, title, body)'
     });
 }
 
@@ -116,11 +144,50 @@ export const deletePost = (req, res) => {
                 });
             }
 
-            if (authUser.role !== process.env.ADMIN_USER_ROLE) {
-                return res.status(403).json({
-                    status: "Unauthorized",
-                    message: "You are not allowed to use this feature"
-                });
+            if (authUser.role === process.env.NORMAL_USER_ROLE) {
+                Post.findById(postid)
+                    .exec()
+                    .then(doc => {
+                        if (doc) {
+                            if (doc.author === authUser.username) {
+                                Post.deleteOne({ _id: postid })
+                                    .exec()
+                                    .then(result => {
+                                        if (result.deletedCount > 0) {
+                                            return res.status(200).json({
+                                                status: "Post successfully deleted",
+                                                message: result
+                                            });
+                                        } else {
+                                            return res.status(404).json({
+                                                status: "Not Found",
+                                                message: "Cannot find a post with the provided id"
+                                            })
+                                        }
+                                    })
+                                    .catch(err => {
+                                        return res.status(500).json({
+                                            Error: err
+                                        })
+                                    })
+                            } else {
+                                return res.status(403).json({
+                                    status: "Unauthorized",
+                                    message: "You are not allowed to delete this post"
+                                })
+                            }
+                        } else {
+                            return res.status(404).json({
+                                status: "Not Found",
+                                message: "Cannot find a post with the id provided"
+                            })
+                        }
+                    })
+                    .catch(err => {
+                        return res.status(500).json({
+                            Error: err
+                        })
+                    })
             }
 
             if (authUser.role === process.env.ADMIN_USER_ROLE) {
@@ -148,10 +215,11 @@ export const deletePost = (req, res) => {
         });
     }
     
-    else res.status(400).json({
-        status: 400,
-        message: 'Supply the usertoken and post id'
-    });
+    else 
+        return res.status(400).json({
+            status: 400,
+            message: 'Supply the usertoken and post id'
+        });
 }
 
 export const updatePost = (req, res) => {
@@ -166,11 +234,48 @@ export const updatePost = (req, res) => {
                 });
             };
 
-            if (authUser.role !== process.env.ADMIN_USER_ROLE) {
-                return res.status(403).json({
-                    status: "Unauthorized",
-                    message: "You are not allowed to use this feature"
-                });
+            if (authUser.role === process.env.NORMAL_USER_ROLE) {
+                Post.find({_id: postid}, (err, post) => {
+                    if (err) {
+                        return res.status(500).json({
+                            status: "Post.find error",
+                            Error: err
+                        })
+                    }
+
+                    if (post.length > 0) {
+                        if (post[0].author === authUser.username) {
+                            title = title || post[0].title;
+                            body = body || post[0].body;
+                            author = post[0].author;
+                            
+                            Post.updateOne({ _id: postid }, { $set: { title, body, author } })
+                                .exec()
+                                .then(result => {
+                                    return res.status(200).json({
+                                        status: "Post updated successfully",
+                                        message: result
+                                    })
+                                })
+                                .catch(err => {
+                                    return res.status(500).json({
+                                        status: "Post.update",
+                                        Error: err
+                                    })
+                                })
+                        } else {
+                            return res.status(403).json({
+                                status: "Unauthorized",
+                                message: "You are not allowed to edit this post"
+                            })
+                        }
+                    } else {
+                        return res.status(404).json({
+                            status: "Not Found",
+                            message: "Post with the provided id not found"
+                        })
+                    }
+                })
             }
 
             if (authUser.role === process.env.ADMIN_USER_ROLE) {
@@ -182,7 +287,7 @@ export const updatePost = (req, res) => {
                         })
                     }
 
-                    if (post) {
+                    if (post.length > 0) {
                         title = title || post[0].title;
                         body = body || post[0].body;
                         author = author || post[0].author;

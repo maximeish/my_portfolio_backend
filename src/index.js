d7f1fec0bdac00ec58b817c62fdac5e7b755f294
Maxime Ishimwe <mxmishimwe5@gmail.com>
2021-07-31 14:20:15 +0200

    replace bodyParser with Express

--- src/index.js
+++ src/index.js
@@ -1,45 +1,46 @@
-import express from 'express';
-import * as bodyParser from 'body-parser';
-import createError from 'http-errors';
-import users from './routes/users.route';
-import posts from './routes/posts.route';
-import messages from './routes/messages.route';
-import postComments from './routes/post-comments.route';
-import auth from './routes/auth.route';
-import blog from './routes/blog.route';
-import mongoose from 'mongoose';
-import dotEnv from 'dotenv';
+import Express from "express";
+import createError from "http-errors";
+import users from "./routes/users.route";
+import posts from "./routes/posts.route";
+import messages from "./routes/messages.route";
+import postComments from "./routes/post-comments.route";
+import auth from "./routes/auth.route";
+import blog from "./routes/blog.route";
+import mongoose from "mongoose";
+import dotEnv from "dotenv";
 
 dotEnv.config();
 
-mongoose.connect(`mongodb+srv://admin-user:${process.env.MONGO_ATLAS_PWD}@blogdata-cluster.9zb8m.mongodb.net/<dbname>?retryWrites=true&w=majority`, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-    useFindAndModify: false
-});
+mongoose.connect(
+    `mongodb+srv://admin-user:${process.env.MONGO_ATLAS_PWD}@blogdata-cluster.9zb8m.mongodb.net/<dbname>?retryWrites=true&w=majority`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    }
+);
 
-const app = express();
+const app = Express();
 const port = process.env.PORT || 3000;
 
 // support parsing of application/json type post data
-app.use(bodyParser.json());
+app.use(Express.json());
 
 //support parsing of application/x-www-form-urlencoded post data
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(Express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.status(200).json({
-    	status: 'success',
-        message: "Welcome to my blog's backend API"
+        status: "success",
+        message: "Welcome to my blog's backend API",
     });
 });
 
-app.use('/', users);
-app.use('/', posts);
-app.use('/', blog);
-app.use('/', messages);
-app.use('/', postComments);
-app.use('/', auth);
+app.use("/", users);
+app.use("/", posts);
+app.use("/", blog);
+app.use("/", messages);
+app.use("/", postComments);
+app.use("/", auth);
 
 app.use((req, res, next) => {
     next(createError(404, "Resource Not Found"));
@@ -47,7 +48,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
     res.status(err.status || 404).json({
-        message: err.message
+        message: err.message,
     });
 });
 
